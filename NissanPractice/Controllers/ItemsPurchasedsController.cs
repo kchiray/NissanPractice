af7cdcc1f6f9cfbc412511dad8650657042d6d2c
kevin chirayath <Batman@DESKTOP-O8058I2>
2018-02-01 14:23:41 -0500

    controllers

--- NissanPractice/Controllers/ItemsPurchasedsController.cs
+++ NissanPractice/Controllers/ItemsPurchasedsController.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using NissanPractice.Models;
 
-namespace NissanPractice.Models
+namespace NissanPractice.Controllers
 {
     public class ItemsPurchasedsController : Controller
     {
