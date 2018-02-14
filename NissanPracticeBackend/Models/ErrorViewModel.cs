fa05e1b36398e8fef45b2521fe944b6150496b29
kevin chirayath <Batman@DESKTOP-O8058I2>
2018-02-14 16:21:55 -0500

    backend added for business manipulation

---
+++ NissanPracticeBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NissanPracticeBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
